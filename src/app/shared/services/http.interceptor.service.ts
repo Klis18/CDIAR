946cffc0914c11ba924583c92b54806a4d32ab26
Luis Alejandro Huayamave Cedeño <hbweyb@gmail.com>
2024-05-12 14:41:16 -0500

    chore: Refactor HTTP interceptor service to use switch statement for status code handling

--- src/app/shared/services/http.interceptor.service.ts
+++ src/app/shared/services/http.interceptor.service.ts
@@ -33,25 +33,33 @@ export class HttpInterceptorService implements HttpInterceptor {
           if (event.body && 'statusCode' in event.body) {
             const statusCode = (event.body as JsonResponse<any>).statusCode;
             const message = (event.body as JsonResponse<any>).message;
-            if (statusCode === 200) {
-              // Código de estado 200 (OK)
-              // Ejemplo: Procesar la respuesta exitosa
-            } else if (statusCode === 400) {
-              // Código de estado 400 (Bad Request)
-              window.alert(message);
-              // Ejemplo: Manejar errores de validación o mostrar un mensaje al usuario
-            } else if (statusCode === 401) {
-              // Código de estado 401 (Unauthorized)
-              // Ejemplo: Redirigir al usuario a la página de inicio de sesión
-              if (window.location.pathname !== '/login') {
-                window.location.href = '/login';
-              }
-            } else if (statusCode === 404) {
-              // Código de estado 404 (Not Found)
-              // Ejemplo: Mostrar un mensaje de "recurso no encontrado" al usuario
-            } else if (statusCode === 500) {
-              // Código de estado 500 (Internal Server Error)
-              // Ejemplo: Mostrar un mensaje genérico de error al usuario o registrar el error en el backend
+            switch (statusCode) {
+              case 200:
+                // Código de estado 200 (OK)
+                // Ejemplo: Procesar la respuesta exitosa
+                break;
+              case 400:
+                // Código de estado 400 (Bad Request)
+                window.alert(message);
+                // Ejemplo: Manejar errores de validación o mostrar un mensaje al usuario
+                break;
+              case 401:
+                // Código de estado 401 (Unauthorized)
+                // Ejemplo: Redirigir al usuario a la página de inicio de sesión
+                if (window.location.pathname !== '/login') {
+                  window.location.href = '/login';
+                }
+                break;
+              case 404:
+                // Código de estado 404 (Not Found)
+                // Ejemplo: Mostrar un mensaje de "recurso no encontrado" al usuario
+                break;
+              case 500:
+                // Código de estado 500 (Internal Server Error)
+                // Ejemplo: Mostrar un mensaje genérico de error al usuario o registrar el error en el backend
+                break;
+              default:
+                break;
             }
           }
         }
