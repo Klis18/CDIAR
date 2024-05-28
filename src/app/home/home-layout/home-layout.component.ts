4e9afa706d25d38140064e9055568a2d1e22d2aa
Klis18 <klisnp18@gmail.com>
2024-05-28 00:39:55 -0500

    fecha y hora en barra de menu

--- src/app/home/home-layout/home-layout.component.ts
+++ src/app/home/home-layout/home-layout.component.ts
@@ -17,6 +17,9 @@ import { AuthService } from '../../auth/services/auth.service';
   styleUrls: ['./home-layout.component.css'],
 })
 export class HomeLayoutComponent implements OnInit, OnDestroy {
+
+  public dateTime:string = '';
+  public customDate: Date = new Date();
   mobileQuery: MediaQueryList;
   private authService = inject(AuthService);
   private router = inject(Router);
@@ -30,6 +33,7 @@ export class HomeLayoutComponent implements OnInit, OnDestroy {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
+  
   }
 
   ngOnInit() {
@@ -37,6 +41,11 @@ export class HomeLayoutComponent implements OnInit, OnDestroy {
       this.userName = user.data.userName;
       this.rol = user.data.rol;
     });
+
+    setInterval(() => {
+      const ahora = new Date();
+      this.dateTime = ahora.toLocaleDateString() + " " + ahora.toLocaleTimeString();
+    }, 1000);
   }
 
   ngOnDestroy(): void {
